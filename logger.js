51648c7815c4692cfa42831fe3419e40c1e77120
Zou Ping <ping.zou@nagra.com>
2018-09-24 16:50:04 +0100

    add -d --debug option. with debug logs.

---
+++ logger.js
@@ -0,0 +1,10 @@
+const winston = require("winston");
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.simple(),
+    transports: [new winston.transports.Console()]
+});
+
+module.exports = {
+    logger: logger
+};
--- mebookdl.js
+++ mebookdl.js
@@ -3,8 +3,9 @@
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const program = require("commander");
-const pjson = require('./package.json');
-const spawn = require('child_process').spawn;
+const pjson = require("./package.json");
+const spawn = require("child_process").spawn;
+const logger = require("./logger.js").logger;
 
 const DEFAULT_EXTENSION = "azw3";
 const SOBOOKS_URL_1 = "sobooks.cc/books";
@@ -13,15 +14,15 @@ const SOBOOKS_URL_2 = "www.sokindle.com/books";
 const extractUrl = (htmlContent, match) => {
     let url = "";
     const parsedHTML = cheerio.load(htmlContent);
-    parsedHTML('a').map((i, link) => {
-        const href = cheerio(link).attr('href');
+    parsedHTML("a").map((i, link) => {
+        const href = cheerio(link).attr("href");
         if (href.includes(match)) {
             url = href;
         }
     });
 
     return url;
-}
+};
 
 const extractMebookDownloadPageUrl = htmlContent => {
     return extractUrl(htmlContent, "download.php");
@@ -37,28 +38,31 @@ const extractSoBooksBaiduPanUrl = htmlContent => {
     const match = "url=";
     const startIndex = panUrl.indexOf(match) + match.length;
     return panUrl.substring(startIndex);
-}
+};
 
 const extractMebookBaiduPanSecret = htmlContent => {
-        // example "网盘密码：百度网盘密码：aagi     天翼云盘密码：5878"
-        let secret = "";
-        const match = "百度网盘密码：";
-        const lenSecret = 4;
-        const parsedHTML = cheerio.load(htmlContent);
-        parsedHTML("p").map((i, text) => {
-            const content = text.children[0];
-            if (content && "data" in content && content.data.includes(match)) {
-                const startIndex = content.data.indexOf(match) + match.length;
-                secret = content.data.substring(startIndex, startIndex + lenSecret);
-            }
-        });
-        return secret;
-}
+    // example "网盘密码：百度网盘密码：aagi     天翼云盘密码：5878"
+    let secret = "";
+    const match = "百度网盘密码：";
+    const lenSecret = 4;
+    const parsedHTML = cheerio.load(htmlContent);
+    parsedHTML("p").map((i, text) => {
+        const content = text.children[0];
+        if (content && "data" in content && content.data.includes(match)) {
+            const startIndex = content.data.indexOf(match) + match.length;
+            logger.debug(
+                `extractMebookBaiduPanSecret - secretText: ${content.data}`
+            );
+            secret = content.data.substring(startIndex, startIndex + lenSecret);
+        }
+    });
+    return secret;
+};
 
 const extractSoBooksBaiduPanSecret = htmlContent => {
     /**
      * example
-     * 
+     *
      * <div class="e-secret">
      *      <strong style="font-size:20px; color:#F00; text-align:center;">提取密码：88kp</strong>
      * </div>
@@ -67,28 +71,34 @@ const extractSoBooksBaiduPanSecret = htmlContent => {
     const match = "提取密码：";
     const lenSecret = 4;
     const parsedHTML = cheerio.load(htmlContent);
-    const secretText = parsedHTML(".e-secret").children().html();
+    const secretText = parsedHTML(".e-secret")
+        .children()
+        .html();
     // example secretText:   &#x63D0;&#x53D6;&#x5BC6;&#x7801;&#xFF1A;88kp
-    const startIndex = 40;  // 5 unicode characters
+    logger.debug(`extractSoBooksBaiduPanSecret - secretText: ${secretText}`);
+    const startIndex = 40; // 5 unicode characters
     secret = secretText.substring(startIndex, startIndex + lenSecret);
     return secret;
-}
+};
 
 const downloadBook = async (url, extension) => {
-    console.log(`Downloading from ${url} with extension ${extension}`);
+    logger.info(`Downloading from ${url} with extension ${extension}`);
     if (url.includes(SOBOOKS_URL_1) || url.includes(SOBOOKS_URL_2)) {
         await downloadFromSoBooks(url, extension);
     } else {
         await downloadFromMebook(url, extension);
     }
-}
+};
 
 const downloadFromSoBooks = async (url, extension) => {
+    logger.debug(
+        `downloadFromSoBooks - downloading ${url} with extension ${extension}`
+    );
     const E_SECRET_KEY = "2018919";
     fetch(url, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
         },
         body: `e_secret_key=${E_SECRET_KEY}`
     })
@@ -96,11 +106,18 @@ const downloadFromSoBooks = async (url, extension) => {
         .then(body => {
             const panUrl = extractSoBooksBaiduPanUrl(body);
             const secret = extractSoBooksBaiduPanSecret(body);
+            logger.debug(
+                `downloadFromSoBooks - panUrl: ${panUrl}, secret: ${secret}`
+            );
+
             downloadFromBaiduPan(panUrl, secret, extension);
         });
-}
+};
 
 const downloadFromMebook = async (url, extension) => {
+    logger.debug(
+        `downloadFromMebook - downloading ${url} with extension ${extension}`
+    );
     fetch(url)
         .then(resp => resp.text())
         .then(body => extractMebookDownloadPageUrl(body))
@@ -109,31 +126,45 @@ const downloadFromMebook = async (url, extension) => {
         .then(body => {
             const panUrl = extractMebookBaiduPanUrl(body);
             const secret = extractMebookBaiduPanSecret(body);
+            logger.debug(
+                `downloadFromMebook - panUrl: ${panUrl}, secret: ${secret}`
+            );
+
             downloadFromBaiduPan(panUrl, secret, extension);
         });
-}
+};
 
 const downloadFromBaiduPan = (panUrl, secret, extension) => {
-    downloadProcess = spawn("python", 
-        ["pan-baidu-download/bddown_cli.py", "download", panUrl, "-S", secret, "-E", extension, "-D", process.cwd()], 
-        { cwd: __dirname, stdio: 'inherit' });
+    logger.debug(`downloadFromBaiduPan - downloading from ${panUrl}`);
+    downloadProcess = spawn(
+        "python",
+        [
+            "pan-baidu-download/bddown_cli.py",
+            "download",
+            panUrl,
+            "-S",
+            secret,
+            "-E",
+            extension,
+            "-D",
+            process.cwd()
+        ],
+        { cwd: __dirname, stdio: "inherit" }
+    );
     downloadProcess.on("exit", exitCode => {
-        console.log(`Downloading process exits with code ${exitCode}`);
+        logger.info(`Downloading process exits with code ${exitCode}`);
     });
     downloadProcess.on("error", error => {
-        console.log(`Downloading process exits with Error ${error}`);
+        logger.info(`Downloading process exits with Error ${error}`);
     });
-}
+};
 
 program
     .version(pjson.version)
-    .option('-u, --url <url>', 'mebook book page URL')
-    .option('-e, --extension <extension>', 'ebook extension, default to azw3')
+    .option("-u, --url <url>", "mebook book page URL")
+    .option("-e, --extension <extension>", "ebook extension, default to azw3")
+    .option("-d, --debug", "debug log")
     .parse(process.argv);
-
-const printHelp = () => {
-    console.log('Usage: node mebookdl --url <mebook book page URL>');
-}
 
 if (!program.url) {
     program.help();
@@ -141,4 +172,8 @@ if (!program.url) {
 }
 
 const extension = program.extension ? program.extension : DEFAULT_EXTENSION;
+if (program.debug) {
+    logger.level = "debug";
+}
+
 downloadBook(program.url, extension);
