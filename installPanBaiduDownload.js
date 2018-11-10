69fbffdc53afae55eb393c221e48bd2cd38b0790
Ping Zou <sg71.cherub@gmail.com>
2018-11-10 12:11:49 +0000

    install Python dependencies while install

--- installPanBaiduDownload.js
+++ installPanBaiduDownload.js
@@ -3,9 +3,11 @@ const fs = require("fs");
 
 let cmd = "";
 if (fs.existsSync("pan-baidu-download")) {
-    cmd = "cd pan-baidu-download && git pull";
+    cmd =
+        "cd pan-baidu-download && git pull && pip install -r requirements.txt";
 } else {
-    cmd = "git clone https://github.com/pz325/pan-baidu-download.git";
+    cmd =
+        "git clone https://github.com/pz325/pan-baidu-download.git && cd pan-baidu-download && pip install -r requirements.txt";
 }
 
 execSync(cmd);
